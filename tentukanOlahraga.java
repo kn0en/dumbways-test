f36ac1ee09ef5aa9235bc78db4dfa9f43121e5eb
kn0en <pratamaoktavian66@gmail.com>
2020-11-28 13:57:53 +0700

    edit name function Soal nomer 1

--- tentukanOlahraga.java
+++ tentukanOlahraga.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class kaloriOlahraga {
+public class tentukanOlahraga {
     public static void main(String[] args) {
       Scanner kaloriO = new Scanner(System.in);
       
